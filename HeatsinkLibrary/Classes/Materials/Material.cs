e299e4a39050f439d21a3ab77fc2961ab73a1648
engineerjames <jamesleearmes@gmail.com>
2016-10-12 19:03:54 -0600

    Cleaned up 'Air' & 'Material' classes.

--- HeatsinkLibrary/Classes/Materials/Material.cs
+++ HeatsinkLibrary/Classes/Materials/Material.cs
@@ -3,29 +3,14 @@ namespace HeatSinkr.Library
 {
     public abstract class Material
     {
-        /// <summary>
-        /// Units of W/m-K
-        /// </summary>
         public virtual double ThermalConductivity { get; set; }
 
-        /// <summary>
-        /// Units of kg/m^3
-        /// </summary>
         public virtual double Density { get; set; }
         
-        /// <summary>
-        /// Units of J/(kg*K)
-        /// </summary>
         public virtual double SpecificHeat { get; set; } 
 
-        /// <summary>
-        /// Units of Pa-s, also known as Absolute Viscosity
-        /// </summary>
         public virtual double DynamicViscosity { get; set; }
 
-        /// <summary>
-        /// Units of m^2/s
-        /// </summary>
         public virtual double KinematicViscosity
         {
             get
@@ -34,14 +19,8 @@ namespace HeatSinkr.Library
             }
         }
 
-        /// <summary>
-        /// Units of NOTHING it's a non-dimensional number
-        /// </summary>
         public virtual double Prandtl { get; }
 
-        /// <summary>
-        /// Units of m^2/s
-        /// </summary>
         public virtual double Diffusivity { get; }
 
         public double Temperature { get; set; }
