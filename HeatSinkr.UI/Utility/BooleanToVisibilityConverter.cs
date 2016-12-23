80ec18f297d7c0c2af4e9ab5110df9abc2a62d0b
engineerjames <jamesleearmes@gmail.com>
2016-12-22 21:07:05 -0700

    Added booleantoVis converter for toggle button control of collapsable sections.

---
+++ HeatSinkr.UI/Utility/BooleanToVisibilityConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace HeatSinkr.UI.Utility
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts from boolean isChecked value to Collapsed or visible
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="language"></param>
+        /// <returns></returns>
+        object IValueConverter.Convert(object value, Type targetType, object parameter, string language)
+        {
+            var isChecked = (bool)value;
+            var returnVal = isChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            return returnVal;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            Visibility visState;
+            var vis = Enum.TryParse<Visibility>((string)value, out visState);
+            return visState;
+        }
+    }
+}
