e7ecea7a2db8f5f9509ec68952e3a724b57987a7
engineerjames <jamesleearmes@gmail.com>
2016-12-23 20:26:07 -0700

    Added Intellisense comments to base heatsink class.

--- HeatsinkLibrary/Classes/Heatsink/Heatsink.cs
+++ HeatsinkLibrary/Classes/Heatsink/Heatsink.cs
@@ -30,21 +30,84 @@
             }
         }
 
+        /// <summary>
+        /// Entrance length [m]
+        /// </summary>
         public abstract double EntranceLength { get; }
+
+        /// <summary>
+        /// Incident flow velocity (Duct velocity) [m/s]
+        /// </summary>
         public abstract double IncidentFlowVelocity { get; }
+
+        /// <summary>
+        /// Channel flow velocity [m/s]
+        /// </summary>
         public abstract double ChannelVelocity { get; }
+
+        /// <summary>
+        /// Hydraulic diameter [m]
+        /// </summary>
         public abstract double HydraulicDiameter { get; }
+
+        /// <summary>
+        /// Flow area incident to the heatsink including the base [m^2]
+        /// </summary>
         public abstract double FlowArea { get; }
+
+        /// <summary>
+        /// Exposed fin area to the flow (on a per fin basis) [m^2]
+        /// </summary>
         public abstract double FinArea { get; }
+
+        /// <summary>
+        /// Total exposed base area to the inlet flow [m^2]
+        /// </summary>
         public abstract double BaseArea { get; }
+
+        /// <summary>
+        /// Reynold's number [Dimensionless]
+        /// </summary>
         public abstract double ReynoldsNumber { get; }
+
+        /// <summary>
+        /// Pressure drop across the heatsink [Pa]
+        /// </summary>
         public abstract double PressureDrop { get; }
+
+        /// <summary>
+        /// Nusselt number [Dimensionless]
+        /// </summary>
         public abstract double Nu { get; }
+
+        /// <summary>
+        /// Fin efficiency [Dimensionless]
+        /// </summary>
         public abstract double FinEfficiency { get; }
+
+        /// <summary>
+        /// Heat transfer coefficient [W/m-K]
+        /// </summary>
         public abstract double HeatTransferCoefficient { get; }
+
+        /// <summary>
+        /// Thermal convective resistance [K/W]
+        /// </summary>
         public abstract double ThermalResistance_Convection { get; }
+
+        /// <summary>
+        /// Conductive thermal resistance [K/W]
+        /// </summary>
         public abstract double ThermalResistance_Conduction { get; }
+
+        /// <summary>
+        /// Caloric thermal reistance [K/W]
+        /// </summary>
         public abstract double ThermalResistance_Caloric { get; }
+
+        /// <summary>
+        /// Spreading resistance [K/W]
+        /// </summary>
         public abstract double ThermalResistance_Spreading { get; }
 
         /// <summary>
